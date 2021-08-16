af569370d76da8f4a76566a3bfae4fb13450c9f4
Kumar Anubhav <anubhav12189@gmail.com>
2021-08-16 17:39:49 +0530

    Update bank_account.py

--- python/problems/bank_account.py
+++ python/problems/bank_account.py
@@ -1,3 +1,8 @@
+# You are given a Bank account having N amount and you are asked to perfrom ADD(credit) and SUBTRACT(debit) operations.
+# After each operation print the amount left in the Bank account. 
+# If the debit amount is greater than current balance print "Insufficient Funds"(without quotes) and the operation is skipped.
+
+
 def main():
     balance = int(input())
     operations = int(input())
